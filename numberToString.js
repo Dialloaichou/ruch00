a78dfa6e6e6a933b2052413e258c37116c7ddb06
ISMAEL9653 <112156554+ISMAEL9653@users.noreply.github.com>
2023-02-12 23:51:24 +0100

    Add files via upload

---
+++ numberToString.js
@@ -0,0 +1,11 @@
+function numberTroString()
+{
+    var Entre='[1,2,3,4,5,6,7,8,9,10]';
+
+    var Sortie='["un" ,"deux","trois","quatre","cinq","six","sept","huit","neuf","dix"]';
+
+    console.log("Entrée : ",Entre);
+
+    console.log("Sortie : ",Sortie);
+}
+numberTroString();
